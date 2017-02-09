3a23d768953ba849ef0f06d14edfff9a55ab0dad
Craig Bilner <craig.bilner@ig.com>
2017-02-09 21:10:12 +0000

    Fix destructured params

--- __testfixtures__/binds-vars.input.js
+++ __testfixtures__/binds-vars.input.js
@@ -13,7 +13,8 @@ export default Ember.Component.extend({
         const h = 8;
         this.notifyAboutSuccess(b, c, f, h);
       }).then((e) => d + d + e)
-        .then((j, ...args) => this.handler(...args, null, undefined));
+        .then((j, ...args) => this.handler(...args, null, undefined))
+        .then(([{ foo: { foo2, foo3 } }, [ { bar: [ { bar1 }, bar2] }, baz] ]) => [foo2, foo3, bar1, bar2]);
     },
   },
 });
--- __testfixtures__/binds-vars.output.js
+++ __testfixtures__/binds-vars.output.js
@@ -6,25 +6,30 @@ export default Ember.Component.extend({
       const a = 1;
       const b = 2;
       const d = 3;
-      user.save().then(this._p4.bind(this, a)).then(this._p3.bind(this, b, f)).then(this._p2.bind(this, d))
+      user.save().then(this._p5.bind(this, a)).then(this._p4.bind(this, b, f)).then(this._p3.bind(this, d))
+        .then(this._p2.bind(this))
         .then(this._p1.bind(this));
     },
   },
 
-  _p1(j, ...args) {
+  _p1([{ foo: { foo2, foo3 } }, [ { bar: [ { bar1 }, bar2] }, baz] ]) {
+    return [foo2, foo3, bar1, bar2];
+  },
+
+  _p2(j, ...args) {
     return this.handler(...args, null, undefined);
   },
 
-  _p2(d, e) {
+  _p3(d, e) {
     return d + d + e;
   },
 
-  _p3(b, f, c) {
+  _p4(b, f, c) {
     const h = 8;
     this.notifyAboutSuccess(b, c, f, h);
   },
 
-  _p4(a) {
+  _p5(a) {
     const g = 5;
     return user.reload(a, g);
   }
